7c1db3ff7d0bd435e671bc934984b36d0e1de510
xwp <xwp_fullstack@163.com>
2018-10-30 19:45:17 +0800

    day02_获取URL路径参数&查询字符串参数

---
+++ reqresp/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from reqresp import views
+
+urlpatterns = [
+    # 定义路由URL时,未命名参数按定义顺序传递给视图
+    # url(r'reqresp/([a-z]+)/(\d{4})/', views.weather, name='reqresp'),
+    # 命名参数按名字传递
+    url(r'reqresp/(?P<city>[a-z]+)/(?P<year>\d{4})/', views.weather, name='reqresp'),
+]
