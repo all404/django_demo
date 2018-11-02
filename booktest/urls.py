27bf213dea430988be3b0030b18c78c04fdcdb20
xwp <xwp_fullstack@163.com>
2018-11-02 15:21:57 +0800

    day04_模板语法

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from booktest import views
 
 urlpatterns = [
-    url(r'^booktest/$', views.BookView.as_view(), name="booktest")
+    url(r'^booktest/$', views.BookView.as_view(), name="booktest"),
+    url(r'^index/$', views.IndexView.as_view()),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -18,3 +18,16 @@ class BookView(View):
 
         """渲染模板简写"""
         return render(request, 'index.html', {"city": "西安"})
+
+
+class IndexView(View):
+    def get(self, request):
+        context = {
+            'city': '北京',
+            'alist': [11, 22, 33, 44],
+            'adict': {
+                'name': 'laowang',
+                'age': 18
+            }
+        }
+        return render(request, 'index.html', context)
