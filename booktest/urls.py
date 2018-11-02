b42a80b0b47c2b2b2816bd4d70c4aa7d46bbb15f
xwp <xwp_fullstack@163.com>
2018-11-02 21:14:01 +0800

    day04_使用Django开发REST接口

--- booktest/urls.py
+++ booktest/urls.py
@@ -5,4 +5,6 @@ from booktest import views
 urlpatterns = [
     url(r'^booktest/$', views.BookView.as_view(), name="booktest"),
     url(r'^index/$', views.IndexView.as_view()),
+    url(r'^books/$', views.BooksAPIView.as_view()),
+    url(r'^book/(?P<pk>\d+)/$', views.BookAPIView.as_view()),
 ]
--- booktest/views.py
+++ booktest/views.py
@@ -1,11 +1,15 @@
+import json
+from datetime import datetime
+
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.template import loader
 from django.views import View
 
-
+from booktest.models import BookInfo
 from .forms import BookForm
 
 
@@ -24,7 +28,6 @@ class BookView(View):
         return render(request, 'book.html', {'form': form})
 
 
-
 class IndexView(View):
     def get(self, request):
         context = {
@@ -36,3 +39,110 @@ class IndexView(View):
             }
         }
         return render(request, 'index.html', context)
+
+
+"""使用Django开发REST接口(使用Postmain测试接口)"""
+class BooksAPIView(View):
+    """查询所有图书,增加图书"""
+    def get(self, request):
+        """
+         获取所有图书
+         路由 GET /books/
+        """
+        queryset = BookInfo.objects.all()
+        book_list = []
+        for book in queryset:
+            b = {
+                'id': book.id,
+                'btitle': book.btitle,
+                'bpub_date': book.bpub_date,
+                'bread': book.bread,
+                'bcomment': book.bcomment,
+                'image': book.image.url if book.image else ''
+            }
+            book_list.append(b)
+        # 如果要传列表, 必须指定safe=False, 详见源码
+        return JsonResponse(book_list, safe=False)
+
+    def post(self,request):
+        """
+        增加图书
+        路由 POST /books/
+        """
+        json_bytes = request.body
+        json_str = json_bytes.decode()
+        json_dict = json.loads(json_str)
+        # 作为测试,详细校验的代码省略
+        book = BookInfo.objects.create(
+            btitle=json_dict.get('btitle'),
+            bpub_date=datetime.strptime(json_dict.get('bpub_date'), '%Y-%m-%d').date()
+        )
+        return JsonResponse(
+            {
+                'id': book.id,
+                'btitle': book.btitle,
+                'bpub_date': book.bpub_date,
+                'bread': book.bread,
+                'bcomment': book.bcomment,
+                'image': book.image if book.image else ''
+            },
+            status=201
+        )
+
+class BookAPIView(View):
+    """获取,修改,删除图书单个图书"""
+    def get(self, request, pk):
+        """
+        获取单个图书信息
+         路由 GET /book/pk
+        """
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except BookInfo.DoesNotExist:
+            return HttpResponse(status=404)
+        return JsonResponse({
+            'id': book.id,
+            'btitle': book.btitle,
+            'bpub_date': book.bpub_date,
+            'bread': book.bread,
+            'bcomment': book.bcomment,
+            'image': book.image if book.image else ''
+        })
+
+    def put(self, request, pk):
+        """
+        修改单个图书信息
+        路由 PUT /book/pk
+        """
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except BookInfo.DoesNotExist:
+            return HttpResponse(status=404)
+
+        json_bytes = request.body
+        json_str = json_bytes.decode()
+        json_dict = json.loads(json_str)
+        # 参数校验代码省略
+        book.btitle = json_dict.get('btitle')
+        book.bpub_date = datetime.strptime(json_dict.get('bpub_date'), '%Y-%m-%d').date()
+        book.save()
+
+        return JsonResponse({
+            'btitle': book.btitle,
+            'bpub_date': book.bpub_date,
+            'bread': book.bread,
+            'bcomment': book.bcomment,
+            'image': book.image if book.image else ''
+        })
+
+    def delete(self, request, pk):
+        """
+        删除单个图书
+         路由 DELETE /book/pk
+        """
+        try:
+            book = BookInfo.objects.get(pk=pk)
+        except BookInfo.DoesNotExist:
+            return HttpResponse(status=404)
+        book.delete()
+        return HttpResponse(status=204)
